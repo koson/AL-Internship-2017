c5867d7c71f5fc302ddf6ad329f9a5f85b2314e8
Twiz <cuciurean.sergiu@yahoo.com>
2017-07-13 17:26:22 +0300

    Removed the old CAN

--- CAN/SW4STM32/STM32F4-Discovery/Debug/Drivers/BSP/Components/cs43l22.d
+++
@@ -1,8 +0,0 @@
-Drivers/BSP/Components/cs43l22.o: \
- D:/users/Drivers/BSP/Components/cs43l22/cs43l22.c \
- D:/users/Drivers/BSP/Components/cs43l22/cs43l22.h \
- d:\users\drivers\bsp\components\common\audio.h
-
-D:/users/Drivers/BSP/Components/cs43l22/cs43l22.h:
-
-d:\users\drivers\bsp\components\common\audio.h:
--- CAN/SW4STM32/STM32F4-Discovery/Debug/Drivers/BSP/Components/lis3dsh.d
+++
@@ -1,8 +0,0 @@
-Drivers/BSP/Components/lis3dsh.o: \
- D:/users/Drivers/BSP/Components/lis3dsh/lis3dsh.c \
- D:/users/Drivers/BSP/Components/lis3dsh/lis3dsh.h \
- d:\users\drivers\bsp\components\common\accelero.h
-
-D:/users/Drivers/BSP/Components/lis3dsh/lis3dsh.h:
-
-d:\users\drivers\bsp\components\common\accelero.h:
