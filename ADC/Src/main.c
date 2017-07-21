64e191b4b3b3f6461571a8f75bcc36c2b5683da2
Stefan Ursu <ursu.stefan.1@gmail.com>
2017-07-21 15:51:12 +0300

    Changed Pins for buttons

--- ADC/Src/main.c
+++ ADC/Src/main.c
@@ -515,8 +515,8 @@ void button_init()
 	GPIO_InitTypeDef GPIO_InitStruct;
 
 	/*Configure GPIO pin Output Level */
-	HAL_GPIO_WritePin(GPIOE, GPIO_PIN_5|GPIO_PIN_6, GPIO_PIN_RESET);
-	GPIO_InitStruct.Pin = GPIO_PIN_5|GPIO_PIN_6;
+	HAL_GPIO_WritePin(GPIOE, GPIO_PIN_5|GPIO_PIN_6|GPIO_PIN_12|GPIO_PIN_13, GPIO_PIN_RESET);
+	GPIO_InitStruct.Pin = GPIO_PIN_5|GPIO_PIN_6|GPIO_PIN_12|GPIO_PIN_13;
 	GPIO_InitStruct.Mode = GPIO_MODE_OUTPUT_PP;
 	GPIO_InitStruct.Pull = GPIO_NOPULL;
 	GPIO_InitStruct.Speed = GPIO_SPEED_FREQ_LOW;
