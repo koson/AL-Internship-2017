53ba65d7e9b41cec2df13974364cdb641e860d45
Stefan Ursu <ursu.stefan.1@gmail.com>
2017-07-21 16:06:04 +0300

    Removed front led port/pin from rear lights

--- Rear Lamp/ADC/Inc/stm32f4_discovery.h
+++ Rear Lamp/ADC/Inc/stm32f4_discovery.h
@@ -71,21 +71,6 @@ typedef enum
   STP0 = 4,
   STP1 = 5,
   STP2 = 6,
-  TRN0 = 7,
-  TRN1 = 8,
-  TRN2 = 9,
-  TRN3 = 10,
-  TRN4 = 11,
-  HBM0 = 12,
-  HBM1 = 13,
-  HBM2 = 14,
-  HBM3 = 15,
-  HBM4 = 16,
-  LBM0 = 17,
-  LBM1 = 18,
-  LBM2 = 19,
-  LBM3 = 20,
-  LBM4 = 21
 } Led_TypeDef;
 
 typedef enum 
@@ -121,7 +106,7 @@ typedef enum
 /** @defgroup STM32F4_DISCOVERY_LOW_LEVEL_LED STM32F4 DISCOVERY LOW LEVEL LED
   * @{
   */
-#define LEDn                             22
+#define LEDn                             7
 
 #define LED4_PIN                         GPIO_PIN_12
 #define LED4_GPIO_PORT                   GPIOD
@@ -198,71 +183,6 @@ typedef enum
 
 #define BUTTONx_GPIO_CLK_DISABLE(__INDEX__)    do{if((__INDEX__) == 0) KEY_BUTTON_GPIO_CLK_DISABLE(); \
                                                  }while(0)
-/**
-  * @}
-  */
-
-/** @defgroup STM32F4_DISCOVERY_LOW_LEVEL_BUS STM32F4 DISCOVERY LOW LEVEL BUS
-  * @{
-  */
-
-/*############################### SPI1 #######################################*/
-#define DISCOVERY_SPIx                              SPI1
-#define DISCOVERY_SPIx_CLK_ENABLE()                 __HAL_RCC_SPI1_CLK_ENABLE()
-#define DISCOVERY_SPIx_GPIO_PORT                    GPIOA                      /* GPIOA */
-#define DISCOVERY_SPIx_AF                           GPIO_AF5_SPI1
-#define DISCOVERY_SPIx_GPIO_CLK_ENABLE()            __HAL_RCC_GPIOA_CLK_ENABLE()
-#define DISCOVERY_SPIx_GPIO_CLK_DISABLE()           __HAL_RCC_GPIOA_CLK_DISABLE()
-#define DISCOVERY_SPIx_SCK_PIN                      GPIO_PIN_5                 /* PA.05 */
-#define DISCOVERY_SPIx_MISO_PIN                     GPIO_PIN_6                 /* PA.06 */
-#define DISCOVERY_SPIx_MOSI_PIN                     GPIO_PIN_7                 /* PA.07 */
-
-/* Maximum Timeout values for flags waiting loops. These timeouts are not based
-   on accurate values, they just guarantee that the application will not remain
-   stuck if the SPI communication is corrupted.
-   You may modify these timeout values depending on CPU frequency and application
-   conditions (interrupts routines ...). */
-#define SPIx_TIMEOUT_MAX                            0x1000 /*<! The value of the maximal timeout for BUS waiting loops */
-
-
-/*############################# I2C1 #########################################*/
-/* I2C clock speed configuration (in Hz) */
-#ifndef BSP_I2C_SPEED
- #define BSP_I2C_SPEED                            100000
-#endif /* BSP_I2C_SPEED */
-
-/* I2C peripheral configuration defines (control interface of the audio codec) */
-#define DISCOVERY_I2Cx                            I2C1
-#define DISCOVERY_I2Cx_CLK_ENABLE()               __HAL_RCC_I2C1_CLK_ENABLE()
-#define DISCOVERY_I2Cx_SCL_SDA_GPIO_CLK_ENABLE()  __HAL_RCC_GPIOB_CLK_ENABLE()
-#define DISCOVERY_I2Cx_SCL_SDA_AF                 GPIO_AF4_I2C1
-#define DISCOVERY_I2Cx_SCL_SDA_GPIO_PORT          GPIOB
-#define DISCOVERY_I2Cx_SCL_PIN                    GPIO_PIN_6
-#define DISCOVERY_I2Cx_SDA_PIN                    GPIO_PIN_9
-
-#define DISCOVERY_I2Cx_FORCE_RESET()              __HAL_RCC_I2C1_FORCE_RESET()
-#define DISCOVERY_I2Cx_RELEASE_RESET()            __HAL_RCC_I2C1_RELEASE_RESET()
-
-/* I2C interrupt requests */
-#define DISCOVERY_I2Cx_EV_IRQn                    I2C1_EV_IRQn
-#define DISCOVERY_I2Cx_ER_IRQn                    I2C1_ER_IRQn
-
-/* Maximum Timeout values for flags waiting loops. These timeouts are not based
-   on accurate values, they just guarantee that the application will not remain
-   stuck if the SPI communication is corrupted.
-   You may modify these timeout values depending on CPU frequency and application
-   conditions (interrupts routines ...). */
-#define I2Cx_TIMEOUT_MAX    0x1000 /*<! The value of the maximal timeout for BUS waiting loops */
-
-
-/*############################# ACCELEROMETER ################################*/
-/* Read/Write command */
-#define READWRITE_CMD                     ((uint8_t)0x80)
-/* Multiple byte read/write command */
-#define MULTIPLEBYTE_CMD                  ((uint8_t)0x40)
-/* Dummy Byte Send by the SPI Master device in order to generate the Clock to the Slave device */
-#define DUMMY_BYTE                        ((uint8_t)0x00)
-
 
 /** @defgroup STM32F4_DISCOVERY_LOW_LEVEL_Exported_Functions STM32F4 DISCOVERY LOW LEVEL Exported Functions
   * @{
@@ -275,25 +195,7 @@ void     BSP_LED_Toggle(Led_TypeDef Led);
 void     BSP_PB_Init(Button_TypeDef Button, ButtonMode_TypeDef Mode);
 uint32_t BSP_PB_GetState(Button_TypeDef Button);
 
-/**
-  * @}
-  */
 
-/**
-  * @}
-  */
-
-/**
-  * @}
-  */
-
-/**
-  * @}
-  */
-
-/**
-  * @}
-  */
 
 #ifdef __cplusplus
 }
